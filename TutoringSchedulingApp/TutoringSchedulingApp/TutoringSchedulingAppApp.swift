a68624900b2bf88782ffbd5f19bee56984720214
Hayden Barogh <hbarogh@gmail.com>
2025-09-03 14:58:31 -0500

    initial commit

---
+++ TutoringSchedulingApp/TutoringSchedulingApp/TutoringSchedulingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutoringSchedulingAppApp.swift
+//  TutoringSchedulingApp
+//
+//  Created by Monica Barogh on 9/3/25.
+//
+
+import SwiftUI
+
+@main
+struct TutoringSchedulingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TutoringSchedulingApp/TutoringSchedulingAppTests/TutoringSchedulingAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  TutoringSchedulingAppTests.swift
+//  TutoringSchedulingAppTests
+//
+//  Created by Monica Barogh on 9/3/25.
+//
+
+import Testing
+
+struct TutoringSchedulingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
